32639645d06afb8ea856aa463c1d72883deb1d17
CHNWhite <baidu88vip@163.com>
2018-10-14 13:56:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ module.exports = {
   school: "jlu",
   //高德路线规划密钥，必须加入https://restapi.amap.com为request合法域名
   //密钥申请地址http://lbs.amap.com/api/javascript-api/summary/
-  key: "71fbbce0036556ac1f7751886422c7ea",
+  key: "",
+  //这里需要自行申请key
   //地图更新地址，用于热修补，无需每次都提交审核
   updateUrl: "https://www.qq.com/json.json",
   //图片CDN域名
